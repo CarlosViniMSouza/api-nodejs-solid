77a4c93f9211e6b0919d5361ceec0a604ad2deb8
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-03-09 20:12:18 -0300

    Estado Atual: Criando projeto Node.js

---
+++ autogit.sh
@@ -0,0 +1,6 @@
+git add -A
+git commit -m "Estado Atual: Criando projeto Node.js"
+git branch -M main
+git remote add origin https://github.com/CarlosViniMSouza/api-nodejs-solid.git
+git push -u origin main
+git status
