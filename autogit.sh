883e1e851a8231aef50a7df90bb91d8059736317
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-03-09 20:29:02 -0300

    Estado Atual: Carregando vars ambiente

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,4 @@
 git add -A
-git commit -m "Estado Atual: Criando projeto Node.js"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/api-nodejs-solid.git
+git commit -m "Estado Atual: Carregando vars ambiente"
 git push -u origin main
 git status
---
+++ src/env/index.ts
@@ -0,0 +1,20 @@
+import 'dotnev/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+    PORT: z.coerce.number().default(3030),
+});
+
+const envValidationSchema = envSchema.safeParse(process.env);
+
+if (envValidationSchema.success === false) {
+    console.error(
+        '❓ Validation Error!',
+        envValidationSchema.error.format()
+    );
+
+    throw new Error('❓ Validation Error!');
+}
+
+export const env = envValidationSchema.data;
